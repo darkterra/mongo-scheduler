55d7cc5f92ba69731600c9aa6c0d77086b2faa75
DarkTerra <darkterra01@gmail.com>
2021-01-11 17:47:32 +0100

    Refactor helper.js & helper.test.js + Move connection function to helper.js + Add new test to improve coverage of helper.js

--- test/helper.test.js
+++ test/helper.test.js
@@ -1,6 +1,9 @@
+'use strict';
+
 const moment     = require('moment');
 const { expect } = require('chai');
-const helper     = require('../lib/helper');
+
+const { connectToDB, buildSchedule, buildEvent, shouldExit, buildError } = require('../lib/helper');
 
 const defaultEvent = {
   name: 'name',
@@ -11,9 +14,64 @@ const defaultEvent = {
   data: { my: 'data' }
 };
 
+
+describe('Connexion Tests', () => {
+  it('should return throw BAD_URL', async () => {
+    const url = 'badArrayConnectionShouldFireError';
+    
+    try {
+      await expect(async () =>  connectToDB({ connection: url, driverOptions: {} })).to.throw();
+    }
+    catch (err) {
+      // console.log('JYO: BAD_URL Yey, we have an client mongo Error !!!');
+    }
+  });
+  it('should return throw RIGHT_URL & BAD_OPTIONS', () => {
+    const url = 'mongodb://localhost:27017/mongo-scheduler-more';
+    
+    try {
+      expect(async () => await connectToDB({ connection: url, driverOptions: {} })).to.throw();
+    }
+    catch (err) {
+      // console.log('JYO: RIGHT_URL & BAD_OPTIONS Yey, we have an client mongo Error !!!');
+    }
+  });
+  it('should return throw BAD_URL_WITH_CREDENTIALS', () => {
+    const url = 'mongodb://AdminJY:*****@localhost:27017/mongo-scheduler-more';
+    
+    try {
+      expect(async () => await connectToDB({ connection: url, driverOptions: {} })).to.throw();
+    }
+    catch (err) {
+      // console.log('JYO: BAD_URL_WITH_CREDENTIALS Yey, we have an client mongo Error !!!');
+    }
+  });
+  it('should return throw ATLAS_URL', () => {
+    const url = 'mongodb+srv://AdminJY:*****@cluster0.1ba3u.mongodb.net/test?authSource=admin&replicaSet=atlas-ao2dxv-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+    
+    try {
+      expect(async () => await connectToDB({ connection: url, driverOptions: {} })).to.throw();
+    }
+    catch (err) {
+      // console.log('JYO: ATLAS_URL Yey, we have an client mongo Error !!!');
+    }
+  });
+  it('should return throw NO_URL', () => {
+    const url = null;
+    
+    try {
+      expect(async () => await connectToDB({ connection: url, driverOptions: {} })).to.throw();
+    }
+    catch (err) {
+      // console.log('JYO: ATLAS_URL Yey, we have an client mongo Error !!!');
+    }
+  });
+});
+
+
 describe('schedule builder', () => {
   it('should return doc to insert', () => {
-    const { doc } = helper.buildSchedule(defaultEvent);
+    const { doc } = buildSchedule(defaultEvent);
 
     doc.should.have.properties({
       name: 'name',
@@ -27,7 +85,7 @@ describe('schedule builder', () => {
   });
 
   it('should return query for updates', () => {
-    const { query } = helper.buildSchedule(defaultEvent);
+    const { query } = buildSchedule(defaultEvent);
     
     query.should.have.properties({
       name: 'name',
@@ -42,11 +100,11 @@ describe('schedule builder', () => {
       expect(newerr).to.be.equal('/!\\ Missing property "name"');
     };
     
-    helper.buildSchedule({}, expectation);
+    buildSchedule({}, expectation);
   });
 
   it('should default to empty conditions', () => {
-    const { doc } = helper.buildSchedule({ name: 'new-event'});
+    const { doc } = buildSchedule({ name: 'new-event'});
     
     doc.should.have.properties({
       name: 'new-event',
@@ -62,13 +120,13 @@ describe('schedule builder', () => {
     const cronDetails = { ...defaultEvent, cron: '0 0 23 * * *' };
 
     it('should include cron string in doc', () => {
-      const { doc } = helper.buildSchedule(cronDetails);
+      const { doc } = buildSchedule(cronDetails);
       
       doc.cron.should.eql('0 0 23 * * *');
     });
 
     it('should calculate next tick', () => {
-      const { doc }       = helper.buildSchedule(cronDetails);
+      const { doc }       = buildSchedule(cronDetails);
       const sanitizedDate = moment(doc.conditions.after).startOf('second');
       
       let nextTick = moment().hours(23).startOf('hour').toDate();
@@ -88,7 +146,7 @@ describe('event builder', () => {
   it('extends query with id from storage', () => {
     const doc = { ...defaultEvent, storage: { id: 'HI!!!' }};
     
-    const event = helper.buildEvent(doc);
+    const event = buildEvent(doc);
     
     event.storage.query._id.should.eql('HI!!!');
   });
@@ -96,37 +154,37 @@ describe('event builder', () => {
   it('returns additional data', () => {
     const doc = { ...defaultEvent, data: 'OMG!' };
 
-    const event = helper.buildEvent(doc);
+    const event = buildEvent(doc);
     event.data.should.eql('OMG!');
   });
 });
 
 describe('should exit', () => {
   it('should return true if an error is passed', () => {
-    helper.shouldExit(new Error()).should.eql(true);
+    shouldExit(new Error()).should.eql(true);
   });
 
   it('should return true if last error object has an err string', () => {
-    helper.shouldExit(null, { lastErrorObject: { err: 'hai' }}).should.eql(true);
+    shouldExit(null, { lastErrorObject: { err: 'hai' }}).should.eql(true);
   });
 
   it('should return false if last error object has no err string', () => {
-    helper.shouldExit(null, { lastErrorObject: {}}).should.eql(false);
+    shouldExit(null, { lastErrorObject: {}}).should.eql(false);
   });
 
   it('should return false if there is no lastErrorObject', () => {
-    helper.shouldExit(null, {}).should.eql(false);
+    shouldExit(null, {}).should.eql(false);
   });
 });
 
 describe('error builder', () => {
   it('should return error', () => {
     const err = new Error();
-    helper.buildError(err).should.equal(err);
+    buildError(err).should.equal(err);
   });
 
   it('should wrap err string in error', () => {
     const result = { lastErrorObject: { err: 'sad times' }};
-    helper.buildError(null, result).message.should.equal('sad times');
+    buildError(null, result).message.should.equal('sad times');
   });
 });
